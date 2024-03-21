e05f4d95ccc0e843dcae967cdf2c7716392867e1
xiaoya <xiaoya.zhu@student.xjtlu.edu.cn>
2024-03-21 23:59:26 +0000

    trick

--- models/networks.py
+++ models/networks.py
@@ -598,7 +598,7 @@ class NLayerDiscriminator(nn.Module):
             nn.LeakyReLU(0.2, True)
         ]
 
-        sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
+        sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]
         self.model = nn.Sequential(*sequence)
 
     def forward(self, input):
